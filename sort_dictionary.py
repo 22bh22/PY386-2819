b79a50d32c9e2febb1adc159b38dba85bb39644b
22bh22 <118910004+22bh22@users.noreply.github.com>
2022-12-20 18:05:20 +0300

    Add files via upload

--- sort_dictionary.py
+++ sort_dictionary.py
@@ -5,21 +5,16 @@ def mixs(num):
     except ValueError:
         return (1, num, '')
 print("Эта программа сортирует словарь по значениям.")
-unsorted_dict = {0: 'Жак-Ив', 1: '11', 2: 10 ** 3, 3:'Black', 4: "060", 5: 0.07, 6: 1, 7: 'Heart', 8:'Кусто', 9:"#"}
-print("Допустим, у нас есть словарь: ", unsorted_dict, ".", sep = "")
+unsorted_dict = {0: 'Жак-Ив', 1: '22', 2: 20 ** 20, 3: 'Black', 4: '060', 5: 0.07, 6: 1, 7: 'Heart', 8: 'Кусто', 9: '#', 10: '22', 11: '123', 15: 22, 14: -5, 13: chr(32) * 4, 21: chr(10)}
+print('Допустим, у нас есть словарь: ', unsorted_dict, '.', sep = '')
 temp_values = []
 for i in unsorted_dict:
 	temp_values.append(unsorted_dict.get(i))
-print("Возьмём из него список значений: ", temp_values, ".", sep = "")
+print('Возьмём из него список значений: ', temp_values, '.', sep = '')
 temp_values.sort(key = mixs)
-#res = sorted(temp_values, key = lambda ele: (0, int(ele))
-#                        if ele.isdigit() else (1, ele))
-#print(res)
-print("Отсортируем: ", temp_values, ".", sep = "")
-#for i, elem in enumerate(temp_values):
-#	print(i, "-", elem)
+print('Отсортируем: ', temp_values, '.', sep = '')
 temp_keys = unsorted_dict.keys()
-print("Теперь возьмём список ключей: ", temp_keys, ".", sep = "")
+print('Теперь возьмём список ключей: ', temp_keys, '.', sep = '')
 sorted_dict = dict(zip(temp_keys, temp_values))
-print("И создадим из двух списков новый отсортированный словарь:\n", sorted_dict, ".", sep = "")
-input("Для завершения нажмите любую клавишу...")
+print('И создадим из двух списков новый отсортированный словарь:\n', sorted_dict, '.', sep = '')
+input('Для завершения нажмите любую клавишу...')
